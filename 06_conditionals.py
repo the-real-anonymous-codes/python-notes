fca6c0b97e442e74a6ac56393f352bea0501dceb
anonymous <techbylalit01@gmail.com>
2026-08-02 13:01:23 +0530

    Update GitHub repository link in comments

--- 06_conditionals.py
+++ 06_conditionals.py
@@ -38,4 +38,4 @@ else:
 # You can also combine multiple conditions in a single if/elif block using 'or' or 'and'.
 
 # For more projects on if-elif-else, visit my repo on GitHub:
-# https://github.com/sirvilalit/if-elif-else-practice
+# https://github.com/the-real-anonymous-codes/if-elif-else-practice
